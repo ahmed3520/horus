c94ea7ca416d5867ad6a74eeba4e419c3a21249a
Ahmed Taha <ataha352000@gmail.com>
2022-11-02 04:23:46 +0200

    fix footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
             </div>
             <div className="whatsapp_float">
             
-             <a aria-label="Chat on WhatsApp" href="https://wa.me/01118067306"> <img alt="Chat on WhatsApp" src="whatsapp-icon.png" /></a>
+             <a aria-label="Chat on WhatsApp" href="https://wa.me/+201008362902"> <img alt="Chat on WhatsApp" src="whatsapp-icon.png" /></a>
 
             
 
@@ -25,18 +25,20 @@ const Footer = () => {
       </section>
 
       <footer>
-        <div className='container'>
-          <div className='box'>
-            <div className='logo'>
-              <img src='../images/logo-light.png' alt='' />
+        <div className='container w-footer'>
+          <div className='box cont-b'>
+            <div className='logo flex-col'>
               <h2>Do You Need Help With Anything?</h2>
               <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
               <div className='input flex'>
-                <input type='text' placeholder='Email Address' />
-                <button>Subscribe</button>
+                <input type='text' className="sub-inp" placeholder='Email Address' />
+                <button className="sub-btn">Subscribe</button>
               </div>
             </div>
           </div>
+          <>
+    {
+    /*          <>
           {footer.map((val) => (
             <div className='box'>
               <h3>{val.title}</h3>
@@ -47,9 +49,44 @@ const Footer = () => {
               </ul>
             </div>
           ))}
+          </>*/
+          }
+          </>
+          <div className='box'>
+              <h3>Your can contact us on these numbers:</h3>
+              <ul>
+                <li>
+                <a href="tel:+201008362902" class="Blondie">
+                    +201008362902
+                </a>
+                </li>
+                <li>
+                  <a href="tel:+8613016424661" class="Blondie">
+                        +8613016424661
+                  </a>
+                </li>
+ 
+              </ul>
+            </div>
+            <div className='box'>
+              <h3>Your can reach us on by mail:</h3>
+              <ul>
+                <li>
+                <a href="mailto:mohamedfathi00@yahoo.com">mohamedfathi00@yahoo.com</a>
+                </li>
+                <li>
+                  <a href="mailto:ahmed.sohail2020@yahoo.com" class="Blondie">
+                  ahmed.sohail2020@yahoo.com
+                  </a>
+                </li>
+ 
+              </ul>
+            </div>
         </div>
+    
+     
       </footer>
-      <div className='legal'>
+      <div className='legal flex-col'>
         <span>© 2022 Soho Tours Egy. Designd By NourEldein.</span>
       </div>
     </>
