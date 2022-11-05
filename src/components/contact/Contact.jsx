c344283ed664b09eb1e6d578cf015e81a17e2691
Ahmed Taha <ataha352000@gmail.com>
2022-11-05 12:35:53 +0200

    fix

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,22 +2,61 @@ import React from "react"
 import img from "../images/pricing.jpg"
 import Back from "../common/Back"
 import "./contact.css"
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const [name, setName] = React.useState("")
+  const [email, setEmail] = React.useState("")
+  const [message, setMessage] = React.useState("")
+  const [error, setError] = React.useState(false)
+  const [success, setSuccess] = React.useState(false)
+const [Subject, setSubject] = React.useState("")
+
+function submitEmail(e){
+  e.preventDefault();
+  if(name === "" || email === "" || message === "" || Subject === ""){
+    setError(true)
+  }else{
+    setError(false)
+  }
+  const templateParams = {
+    from_name: name,
+    message: message,
+    user_email: email,
+    subject: Subject,
+};
+emailjs.send(process.env.REACT_APP_EMAILJS_SERVICE_ID,'template_srz0m6h', templateParams, process.env.REACT_APP_EMAILJS_PUBLIC_KEY, process.env.REACT_APP_EMAILJS)
+.then((response) => {
+   console.log('SUCCESS!', response.status, response.text);
+    setSuccess(true)
+}, (err) => {
+   console.log('FAILED...', err);
+});
+}
+React.useEffect(() => {
+  if(success){
+    setName("")
+    setEmail("")
+    setMessage("")
+    setSubject("")
+  }
+}, [success])
   return (
     <>
       <section className='contact mb'>
         <Back name='Contact Us' title='Get Helps & Friendly Support' cover={img} />
         <div className='container'>
-          <form className='shadow' action="https://formspree.io/f/xbjbavkv" method="POST">
+          <form className='shadow' onSubmit={submitEmail} >
             <h4>Fillup The Form</h4> <br />
             <div>
-              <input type='text' placeholder='Name' name='Name' />
-              <input type='text' placeholder='Email' name='Email' />
+              <input type='text' placeholder='Name' name='Name' onChange={e=>setName(e.target.value)} />
+              <input type='text' placeholder='Email' name='Email' onChange={e=>setEmail(e.target.value)}/>
             </div>
-            <input type='text' placeholder='Subject' name='Subject' />
-            <textarea cols='30' rows='10'></textarea>
-            <button>Submit Request</button>
+            <input type='text' placeholder='Subject' name='Subject' onChange={e=>setSubject(e.target.value)} />
+            <textarea cols='30' rows='10' onChange={e=>setMessage(e.target.value)}></textarea>
+            {error && <p className='error'>All Fields are required!</p>}
+            {success && <p className='success'>Message Sent Successfully!</p>}
+            <button type="submit">Submit Request</button>
           </form>
         </div>
       </section>
